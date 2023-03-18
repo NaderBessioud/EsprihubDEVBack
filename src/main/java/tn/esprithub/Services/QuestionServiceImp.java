e6f6c992ed9e718c7b9eba6cdb22226e8afef97c
NaderBessioud <79223546+NaderBessioud@users.noreply.github.com>
2023-03-18 18:52:02 +0100

    Update QuestionServiceImp.java

--- src/main/java/tn/esprithub/Services/QuestionServiceImp.java
+++ src/main/java/tn/esprithub/Services/QuestionServiceImp.java
@@ -467,7 +467,7 @@ public class QuestionServiceImp implements QuestionService {
 		 byte[] data;
 		  try {
       		
-       	   ftpClient.connect("192.168.1.16", 21);
+       	   ftpClient.connect("ftp-server", 21);
 		         ftpClient.login("ftp-user", "ftpuser");
            ftpClient.enterLocalPassiveMode();
            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
