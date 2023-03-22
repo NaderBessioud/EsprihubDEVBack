100a94ba6d8126fb560fa58bb0ba4f7fcaa137fd
NaderBessioud <79223546+NaderBessioud@users.noreply.github.com>
2023-03-22 11:04:14 +0100

    Update ResponseServiceImp.java

--- src/main/java/tn/esprithub/Services/ResponseServiceImp.java
+++ src/main/java/tn/esprithub/Services/ResponseServiceImp.java
@@ -203,7 +203,7 @@ public class ResponseServiceImp implements IResponseService {
 		 byte[] data;
 		  try {
       		
-       	   ftpClient.connect("192.168.2.181", 21);
+       	   ftpClient.connect("172.19.0.3", 21);
 		         ftpClient.login("ftp-user", "ftpuser");
            ftpClient.enterLocalPassiveMode();
            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
